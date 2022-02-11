453654e2fe2f96127ea08bdd60a450425a28576c
FunnCo <41015524+FunnCo@users.noreply.github.com>
2022-02-11 12:02:52 +0300

    Revert "Init commit"
    This reverts commit 469f0cfd5457d8b9a229519f9e1d0b305c163d81.

--- Git_LR1/Git_LR1.py
+++ Git_LR1/Git_LR1.py
@@ -1,9 +1,2 @@
 
 print("Hello Python from Visual Studio!") 
-
-factorial = 1
-for i in range(1,15):
-    factorial = i*factorial
-    print("Current factorial is:", factorial)
-
-print("\nresult factorial is:", factorial)
